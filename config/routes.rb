6322f2cf1e5b6f72575aac593ee59457347e8621
bpereira74 <bpereira74@hotmail.com>
2023-10-14 09:30:38 -0300

    feat: implementcion nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+      get '/home',    to: 'pages#home'
+      get '/privacy', to: 'pages#privacy'
+      get '/legal',   to: 'pages#terms'
+      root 'pages#home'
 end
