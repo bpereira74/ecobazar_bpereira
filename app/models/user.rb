80b9c9d23c20da6a04cdec8532757cabf9ca3de6
bpereira74 <bpereira74@hotmail.com>
2023-10-14 10:01:18 -0300

    feat: vista de devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,4 +19,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Validaciones
+  validates :name, presence: true
+  validates :phone, presence: true
+  enum role: [:regular, :admin]
 end
